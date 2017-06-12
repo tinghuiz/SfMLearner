049606ae6e777d4892d36245387bb95244e4561d
Tinghui Zhou <tinghuiz@eecs.berkeley.edu>
2017-06-11 19:46:20 -0700

    Entry code for training

---
+++ train.py
@@ -0,0 +1,43 @@
+from __future__ import division
+import tensorflow as tf
+import pprint
+import random
+import numpy as np
+from SfMLearner import SfMLearner
+import os
+
+flags = tf.app.flags
+flags.DEFINE_string("dataset_dir", "", "Dataset directory")
+flags.DEFINE_string("checkpoint_dir", "./checkpoints/", "Directory name to save the checkpoints")
+flags.DEFINE_float("learning_rate", 0.0002, "Learning rate of for adam")
+flags.DEFINE_float("beta1", 0.9, "Momentum term of adam")
+flags.DEFINE_float("smooth_weight", 0.0, "Weight for smoothness")
+flags.DEFINE_float("explain_reg_weight", 0.0, "Weight for explanability regularization")
+flags.DEFINE_integer("batch_size", 4, "The size of of a sample batch")
+flags.DEFINE_integer("img_height", 128, "Image height")
+flags.DEFINE_integer("img_width", 416, "Image width")
+flags.DEFINE_integer("seq_length", 3, "Sequence length for each example")
+flags.DEFINE_integer("max_steps", 200000, "Maximum number of training iterations")
+flags.DEFINE_integer("summary_freq", 100, "Logging every log_freq iterations")
+flags.DEFINE_integer("save_latest_freq", 5000, \
+    "Save the latest model every save_latest_freq iterations (overwrites the previous latest model)")
+flags.DEFINE_boolean("continue_train", False, "Continue training from previous checkpoint")
+FLAGS = flags.FLAGS
+
+def main(_):
+    seed = 8964
+    tf.set_random_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+    pp = pprint.PrettyPrinter()
+    pp.pprint(flags.FLAGS.__flags)
+    
+    if not os.path.exists(FLAGS.checkpoint_dir):
+        os.makedirs(FLAGS.checkpoint_dir)
+        
+    sfm = SfMLearner()
+    sfm.train(FLAGS)
+
+if __name__ == '__main__':
+    tf.app.run()
